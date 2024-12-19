88650d261cd2bf6fc3f23511d5da005d82a6ea8e
Jorgemrqz <166621479+Jorgemrqz@users.noreply.github.com>
2024-12-19 15:43:15 -0500

    exponiendo el puerto

--- books-crud-backend/index.js
+++ books-crud-backend/index.js
@@ -115,6 +115,6 @@ app.delete('/api/books/:id', (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
